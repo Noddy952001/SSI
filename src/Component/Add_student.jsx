2638fe0c8ea4a00dcbee8475172755bbcd565c3a
vikas <vikasnwrdr2001@gmail.com>
2022-09-13 15:59:08 +0530

    set add student funtionlity

--- src/Component/Add_student.jsx
+++ src/Component/Add_student.jsx
@@ -1,10 +1,87 @@
 import React from "react";
+import axios from "axios"
+import { useState } from "react";
+
 
 
 export const Add_Student = () => {
+
+    const  [firstname , setfirstname]  = useState()
+    const  [Lastname , setLastname]  = useState()
+    const  [email , setemail]  = useState()
+    const  [address , setaddress]  = useState()
+
+ 
+    const data = {
+        First_name : firstname,
+        Last_name : Lastname,
+        Email : email,
+        Address : address
+
+    }
+       
+    const handelSubmit = () => {
+
+        axios.post("https://studentddata.herokuapp.com/student", data).then(function(res){
+            console.log(res.data)
+        })
+
+    }
+
+
+
     return(
         <div>
             <h1>Add_Student</h1>
+        
+            <label>First Name</label>
+            <input 
+                type="text"
+                placeholder="Enter FirsrName"
+                onChange={(e) => {
+                    setfirstname(e.target.value)
+                }}
+            /> 
+            <br />
+
+
+            <label>Last Name</label>
+            <input 
+                type="text"
+                placeholder="Enter lastName"
+                onChange={(e) => {
+                    setLastname(e.target.value)
+                }}
+            /> 
+            <br />
+
+
+            <label>Email</label>
+            <input 
+                type="text"
+                placeholder="Enter Email"
+                onChange={(e) => {
+                    setemail(e.target.value)
+                }}
+            /> 
+            <br />
+
+
+
+            <label>Address</label>
+            <input 
+                type="text"
+                placeholder="Enter Address"
+                onChange={(e) => {
+                    setaddress(e.target.value)
+                }}
+            /> 
+            <br />
+
+
+            <button
+                onClick={handelSubmit}
+            >Add</button>
         </div>
     )
 }
