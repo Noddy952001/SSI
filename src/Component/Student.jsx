b44d3ae6a404c4d031d0d6164a8444f1a7b9dc98
vikas <vikasnwrdr2001@gmail.com>
2022-09-13 15:46:08 +0530

    get all students details

--- src/Component/Student.jsx
+++ src/Component/Student.jsx
@@ -15,7 +15,7 @@ export const Student = () => {
 
     const Data = () => {
         axios.get("https://studentddata.herokuapp.com/student").then(function(res){
-            // setData(res.data)
+            setData(res.data)
             console.log("avid" , res.data)
         })
     }
@@ -24,13 +24,38 @@ export const Student = () => {
 
     return(
 
-
-
-
         <div>
-            <h1>Student</h1>
 
 
+
+            <div className="maindiv">
+                <table className="table">
+                        <thead>
+                            <tr>
+                                <th className="table">No</th>
+                                <th  className="table">First name</th>
+                                <th  className="table">Lastname</th>
+                                <th  className="table">Email</th>
+                                <th  className="table">Address</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data?.map((el , i=0 ) => {
+                                return (
+                                    <tr>
+                                        <td className="table">{i+1}</td>
+                                        <td className="table">{el.First_name}</td>
+                                        <td className="table">{el.Last_name}</td>
+                                        <td className="table">{el.Email}</td>
+                                        <td className="table">{el.Address}</td>
+                                    </tr>
+                                )
+                                })
+                            }
+                        </tbody>
+                </table>
+            </div>
 
         </div>
     )
