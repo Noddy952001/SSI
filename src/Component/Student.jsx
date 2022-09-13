d7bc02e19653c167405aea2b524ad82d7e4ea4bb
vikas <vikasnwrdr2001@gmail.com>
2022-09-13 15:31:26 +0530

    fetch api data

--- src/Component/Student.jsx
+++ src/Component/Student.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import axios from "axios"
+import { useEffect } from "react";
+import { useState } from "react";
+
 
 
 export const Student = () => {
+
+    const [data , setData] = useState()
+
+    useEffect(() => {
+        Data()
+    },[])
+
+    const Data = () => {
+        axios.get("https://studentddata.herokuapp.com/student").then(function(res){
+            // setData(res.data)
+            console.log("avid" , res.data)
+        })
+    }
+
+
+
     return(
+
+
+
+
         <div>
             <h1>Student</h1>
+
+
+
         </div>
     )
 }
